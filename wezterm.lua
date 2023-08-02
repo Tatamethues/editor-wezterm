49585d2fb5e25829b5288779d0f0776457c50353
Si <34636866+Tatamethues@users.noreply.github.com>
2023-08-03 00:47:37 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -8,16 +8,16 @@ local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
-
+config.font = wezterm.font "CaskaydiaCove NFM"
 config.font_size = 14
 config.color_scheme = "Catppuccin Mocha"
+
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom =0 ,
 }
-
 -- Tab
 config.tab_bar_at_bottom = true
 
